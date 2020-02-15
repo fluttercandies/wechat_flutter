b7e97654b789b7b3d7fcff39c2d2796dfe719393
ahyangnb <zonggeu@gmail.com>
2020-02-16 00:00:17 +0800

    commit cached_network_image: 2.0.0

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -6,5 +6,6 @@ export "FLUTTER_TARGET=/Users/q1/Documents/gitPro/ahyangnb/wechat_flutter/lib/ma
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/q1/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=0.0.5"
+export "FLUTTER_BUILD_NAME=0.0.6"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
