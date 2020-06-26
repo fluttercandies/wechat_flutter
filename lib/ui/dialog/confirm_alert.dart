bac34af5a4022f98a52a1ec0f777de4d42635831
ahyangnb <zonggeu@gmail.com>
2020-06-26 17:53:05 +0800

    commit 提示对话框优化

--- lib/ui/dialog/confirm_alert.dart
+++ lib/ui/dialog/confirm_alert.dart
@@ -1,19 +1,19 @@
 import 'package:wechat_flutter/tools/wechat_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:wechat_flutter/ui/flutter/my_cupertino_dialog.dart';
 
 void confirmAlert<T>(
-    BuildContext context,
-    VoidCallbackConfirm callBack, {
-      int type,
-      String tips,
-      String okBtn,
-      String cancelBtn,
-      TextStyle okBtnStyle,
-      TextStyle style,
-      bool isWarm = false,
-      String warmStr,
-    }) {
+  BuildContext context,
+  VoidCallbackConfirm callBack, {
+  int type,
+  String tips,
+  String okBtn,
+  String cancelBtn,
+  TextStyle okBtnStyle,
+  TextStyle style,
+  bool isWarm = false,
+  String warmStr,
+}) {
   showDialog<T>(
     context: context,
     barrierDismissible: false,
@@ -21,42 +21,43 @@ void confirmAlert<T>(
       if (!strNoEmpty(okBtn)) okBtn = '确定';
       if (!strNoEmpty(cancelBtn)) cancelBtn = '取消';
       if (!strNoEmpty(warmStr)) warmStr = '温馨提示：';
-      return CupertinoAlertDialog(
+      return MyCupertinoAlertDialog(
         title: isWarm
             ? new Padding(
-          padding: EdgeInsets.only(bottom: 20.0, top: 0),
-          child: Text(
-            '$warmStr',
-            style: TextStyle(
-                color: Color(0xff343243),
-                fontSize: 19.0,
-                fontWeight: FontWeight.normal),
-          ),
-        )
+                padding: EdgeInsets.only(bottom: 20.0, top: 0),
+                child: Text(
+                  '$warmStr',
+                  style: TextStyle(
+                      color: Color(0xff343243),
+                      fontSize: 19.0,
+                      fontWeight: FontWeight.normal),
+                ),
+              )
             : new Padding(
-          padding: EdgeInsets.symmetric(vertical: 10.0),
-          child: Text(
-            '$tips',
-            style: TextStyle(
-                color: Color(0xff343243),
-                fontSize: 19.0,
-                fontWeight: FontWeight.normal),
-          ),
-        ),
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  '$tips',
+                  style: TextStyle(
+                      color: Color(0xff343243),
+                      fontSize: 19.0,
+                      fontWeight: FontWeight.normal),
+                ),
+              ),
         content: isWarm
             ? new Padding(
-          padding: EdgeInsets.only(bottom: 10.0),
-          child: Text(
-            '$tips',
-            style: TextStyle(color: Color(0xff888697)),
-          ),
-        )
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Text(
+                  '$tips',
+                  style: TextStyle(color: Color(0xff888697)),
+                ),
+              )
             : new Container(),
         actions: <Widget>[
           CupertinoDialogAction(
             child: new Text(
               '$cancelBtn',
-              style: TextStyle(fontWeight: FontWeight.w400),
+              style:
+                  TextStyle(fontWeight: FontWeight.w400, color: Colors.black),
             ),
             isDefaultAction: true,
             onPressed: () {
